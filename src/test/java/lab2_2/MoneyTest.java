8ad0f0cd26788c5262368a7b53a9e30ff19d411b
Sebastian <seba52a@gmail.com>
2016-01-22 20:35:08 +0100

    Dodanie 3 testow
    porownajDwaObiektyOTakiejSamejWartosci
    porownajDwaObiektyORoznejWartosci
    multiplyTest

--- src/test/java/lab2_2/MoneyTest.java
+++ src/test/java/lab2_2/MoneyTest.java
@@ -3,12 +3,36 @@ package lab2_2;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class MoneyTest {
+	
+	Money pieniadzJeden;
+	Money pieniadzDwa;
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void porownajDwaObiektyOTakiejSamejWartosci() {
+		
+		pieniadzJeden = new Money(12.34, "PLN");
+		pieniadzDwa = new Money(12.34, "PLN");
+		assertThat(pieniadzJeden, is(equalTo(pieniadzDwa)));
 	}
-
+	
+	@Test
+	public void porownajDwaObiektyORoznejWartosci() {
+		
+		pieniadzJeden = new Money(12.35, "PLN");
+		pieniadzDwa = new Money(12.34, "PLN");
+		assertThat(pieniadzJeden, is(not(equalTo(pieniadzDwa))));
+	}
+	
+	@Test
+	public void multiplyTest() {
+		pieniadzJeden = new Money(9,"PLN");
+		pieniadzDwa = new Money(90,"PLN");
+		
+		assertThat(pieniadzJeden.multiplyBy(10),is(equalTo(pieniadzDwa)));
+	}
 }
